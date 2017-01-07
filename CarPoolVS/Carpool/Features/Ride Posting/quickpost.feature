12f06e79ed5c7bfe61a962b5fd42b6fa700c89c1
Naveen <naveenhome@gmail.com>
2017-01-07 06:19:31 -0800

    Added Specflow

---
+++ CarPoolVS/Carpool/Features/Ride Posting/quickpost.feature
@@ -0,0 +1,18 @@
+﻿Feature: quickpost
+	Ir order to share my car
+	As an employee
+	I want to post my ride with others
+
+Scenario Outline: Quickpost testing
+Given <empname> enter <source> and <destination> and <time>
+And his <email> and <phone>
+When click on <button>
+Then system display <message> with <actionButton> button
+And click on OK button will redirect on <page>
+Examples: 
+| empname  | source   | destination    | time   | email                  | phone        | button  | message        | actionButton | page   |
+| "Naveen" | "Office" | "Kammanahalli" | "5:30" | "naveenhome@gmail.com" | "9810547500" | "Share" | "Ride shared." | "Ok"         | "Home" |
+| "Naveen" | "Office" | "Kammanahalli" | "5:30" | "naveenhome@gmail.com" | " " | "Share" | "Phone No is required." | "Ok"         | "Quick Post" |
+
+
+
---
+++ CarPoolVS/Carpool/QuickpostSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Carpool
+{
+    [Binding]
+    public class QuickpostSteps
+    {
+        [Given(@"""(.*)"" enter ""(.*)"" and ""(.*)"" and ""(.*)""")]
+        public void GivenEnterAndAnd(string p0, string p1, string p2, string p3)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"his ""(.*)"" and ""(.*)""")]
+        public void GivenHisAnd(string p0, Decimal p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"click on ""(.*)""")]
+        public void WhenClickOn(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"system display ""(.*)"" with ""(.*)"" button")]
+        public void ThenSystemDisplayWithButton(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"click on OK button will redirect on ""(.*)""")]
+        public void ThenClickOnOKButtonWillRedirectOn(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
